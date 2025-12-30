85f9ef4800b3eba34eb26604560d237a16217138
haibonlp <4910399+haibonlp@users.noreply.github.com>
2025-12-30 00:24:26 +0000

    Deploying to gh-pages from @ haibonlp/haibonlp.github.io@2ad4dfcbebb1e04dc501103c74d4e1f158c73dac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,15 +384,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-organized-the-kdd-workshop-on-automatic-prompt-optimization",
+            },},{id: "news-organized-the-kdd-workshop-on-automatic-prompt-optimization",
           title: 'Organized the KDD Workshop on Automatic Prompt Optimization',
           description: "",
           section: "News",},{id: "news-launched-amazon-bedrock-agentcore-evaluations-preview-for-agent-performance-monitoring",
